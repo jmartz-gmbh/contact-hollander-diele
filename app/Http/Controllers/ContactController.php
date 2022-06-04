bb677b8723680d05785f8fd8c44d32a0eae4facc
Jonathan Martz <git@jmartz.de>
2022-06-04 23:33:09 +0200

    add test json response

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
@@ -14,5 +15,9 @@ class ContactController extends Controller
               ->setBody('Hi, welcome user!');
           });
           
+          return response()->json([
+            'name' => 'Abigail',
+            'state' => 'CA',
+        ]);
     }
 }
