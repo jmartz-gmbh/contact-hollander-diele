3f81566b61c81a05c79e8d997dea470535f79217
Jonathan Martz <git@jmartz.de>
2022-06-04 23:39:03 +0200

    update mail test

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,20 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
-    public function send(){
-        Mail::send([], [], function ($message) {
-            $message->to('test@jmartz.de')
-              ->subject('Kontaktformular')
-              ->setBody('Hi, welcome user!');
-          });
-          
-          return response()->json([
+    public function send()
+    {
+        $arrayEmails = ['test@jmartz.de'];
+        $emailSubject = 'My Subject';
+        $emailBody = 'Hello, this is my message content.';
+
+        Mail::send(
+            'emails.normal',
+            ['msg' => $emailBody],
+            function ($message) use ($arrayEmails, $emailSubject) {
+                $message->to($arrayEmails)
+                    ->subject($emailSubject);
+            }
+        );
+
+        return response()->json([
             'name' => 'Abigail',
             'state' => 'CA',
         ]);
